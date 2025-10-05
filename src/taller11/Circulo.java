f24213fbeada47020d6bfb783315c3205181aa6d
mapeflo <mpenarandaf@unicartagena.edu.co>
2025-10-05 17:21:08 -0500

    Avance taller 11

---
+++ src/taller11/Circulo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller11;
+
+/**
+ *
+ * @author marya
+ */
+public class Circulo extends Figura {
+
+   double radio;
+   
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+   
+   @Override
+    public double calcularArea(){
+        double area = Math.PI*(radio*radio);
+        System.out.println("El area del circulo es: " + area);   
+        return  area;
+    }  
+}
---
+++ src/taller11/Figura.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller11;
+
+/**
+ *
+ * @author marya
+ */
+public abstract class Figura {
+    
+    public abstract double calcularArea();
+    
+    public void mostrarArea(){
+        System.out.println("El area de la figura es: " + calcularArea());
+    }   
+}
+    
